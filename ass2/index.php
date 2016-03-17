0eef6ce2c5f883e2ad048fb4a882578a34aea518
geomsaek <msaliba.6@gmail.com>
2016-03-17 11:19:17 +1100

    ass2 completion

--- ass2/index.php
+++ ass2/index.php
@@ -1,8 +1,7 @@
 <!doctype>
-
 <html>
 <head>
-    <title>Company Page</title>
+    <title>Car Search Page</title>
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
@@ -11,51 +10,6 @@
 </head>
 
 <body>
-
-    <div id="myModal" class="modal fade" role="dialog">
-        <div class="modal-dialog">
-
-            <!-- Modal content-->
-            <div class="modal-content">
-                <div class="container-fluid">
-                    <div class="row">
-                        <form method="POST" action="interest.php">
-                            <div class="container-fluid form-row">
-                                <h3>Express your interest</h3>
-                            </div>
-                            <div class="container-fluid form-row">
-                                <label>Your Name</label>
-                                <input type="text" class="form-control" id="express-name" name="interest-name" />
-                            </div>
-
-                            <div class="container-fluid form-row">
-                                <label>Plate Number</label>
-                                <input type="text" class="form-control" id="express-licence-num" name="interest-lic-num" />
-                            </div>
-
-                            <div class="container-fluid form-row">
-                                <label>Proposed Price</label>
-                                <input type="text" class="form-control" id="express-price" name="interest-price" />
-                            </div>
-
-                            <div class="container-fluid form-row">
-                                <label>Contact Number</label>
-                                <input type="text" class="form-control" id="express-contact" name="interest-contact" />
-                            </div>
-
-                            <div class="container-fluid form-row">
-                                <input type="submit" name="express-interest" value="Express Interest" class="btn btn-primary" />
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </div>
-            </div>
-
-        </div>
-    </div>
 
     <header>
         <?php if(isset($_GET['enquiry'])): ?>
@@ -74,21 +28,68 @@
 
     </header>
 
-    <?php if(isset($_POST['submit-search'])):
+    <?php
+
+    if(isset($_POST['submit-search'])):
 
         $userSearch = array();
         $userSearch[0] = $_POST['search-lic-num'];
         $userSearch[1] = $_POST['search-owners'];
-        $userSearch[2] = $_POST['search-lic-num'];
+        $userSearch[2] = $_POST['search-service-list'];
         $userSearch[3] = false;
         if($_POST['search-type'] != "basic"):
             $userSearch[3] = true;
         endif;
 
-
         $results= open_read_file();
 
         if(sizeof($results) > 0): ?>
+
+            <div id="myModal" class="modal fade" role="dialog">
+                <div class="modal-dialog">
+
+                    <!-- Modal content-->
+                    <div class="modal-content">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <form method="POST" action="interest.php">
+                                    <div class="container-fluid form-row">
+                                        <h3>Express your interest</h3>
+                                    </div>
+                                    <div class="container-fluid form-row">
+                                        <label>Your Name</label>
+                                        <input type="text" class="form-control" id="express-name" name="interest-name" />
+                                    </div>
+
+                                    <div class="container-fluid form-row">
+                                        <label>Plate Number</label>
+                                        <input type="text" class="form-control" id="express-licence-num" name="interest-lic-num" />
+                                    </div>
+
+                                    <div class="container-fluid form-row">
+                                        <label>Proposed Price</label>
+                                        <input type="text" class="form-control" id="express-price" name="interest-price" />
+                                    </div>
+
+                                    <div class="container-fluid form-row">
+                                        <label>Contact Number</label>
+                                        <input type="text" class="form-control" id="express-contact" name="interest-contact" />
+                                    </div>
+
+                                    <div class="container-fluid form-row">
+                                        <input type="submit" name="express-interest" value="Express Interest" class="btn btn-primary" />
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
             <section>
                 <div class="container-fluid">
                     <div class="container">
@@ -122,16 +123,16 @@
     <?php else: ?>
             <section>
 
-                <div class="container-fluid">
+                <div class="container-fluid home-wrap">
                     <div class="container">
                         <div class="row">
                             <div class="col-sm-12">
                                 <h1>Search Cars</h1>
 
-                                <div class="container-fluid">
+                                <div class="container-fluid instruction">
                                     <p>Please enter the details of your search</p>
                                 </div>
-                                <form id="car-search-form" action="" method="POST">
+                                <form id="car-search-form" method="POST">
                                     <div class="container-fluid">
                                         <label>Licence Plate Number</label>
                                         <input type="text" class="form-control" id="licence-num" name="search-lic-num" />
@@ -144,7 +145,7 @@
 
                                     <div class="container-fluid">
                                         <label>Services</label>
-                                        <input type="text" class="form-control" id="service-list" name="search-lic-num" />
+                                        <input type="text" class="form-control" id="service-list" name="search-service-list" />
                                     </div>
 
                                     <div class="container-fluid">
--- ass2/search.php
+++ ass2/search.php
@@ -31,40 +31,162 @@
     function display_results($details, $userSearch){
 
         $temp;
+        $resCount = 0;
 
         for($i = 0; $i < sizeof($details); $i++){
 
             $temp = array();
             $temp = explode(":",$details[$i]);
 
-            echo "<div class='container result-wrap'>";
-                echo "<div class='row'>";
-                    echo "<div class='col-sm-12 col-md-6'>";
-                        echo "<p class='seller-name'><strong>Name: </strong>" . $temp[0] . " " . $temp[1] . "</p>";
-                        echo "<p class='seller-miles'><strong>Car Mileage: </strong>" . $temp[2] . "</p>";
-                        echo "<p class='seller-price'><strong>Price: </strong>" . $temp[3] . "</p>";
+            if(check_record($temp,$userSearch)):
 
-                        if($userSearch[3]) {
-                            echo "<p class='seller-contact'><strong>Contact Number: </strong>" . $temp[4] . "</p>";
-                            echo "<p class='seller-owner-count'><strong>Owner Count: </strong>" . $temp[6] . "</p>";
-                            echo "<p class='seller-service'><strong>Service List: </strong>" . $temp[7] . "</p>";
-                        }
+                echo "<div class='container result-wrap'>";
+                    echo "<div class='row'>";
+                        echo "<div class='col-sm-12 col-md-6'>";
+                            echo "<div class='col-md-6 car-image-wrap'>";
+                                echo "<img src='images/" . $temp[10] . "' />";
+                            echo "</div>";
+                            echo "<div class='col-md-6 car-content-wrap'>";
+                                echo "<p class='seller-name'><strong>Name: </strong>" . $temp[0] . " " . $temp[1] . "</p>";
+                                echo "<p class='seller-miles'><strong>Car Mileage: </strong>" . $temp[2] . "</p>";
+                                echo "<p class='seller-price'><strong>Price: </strong>" . $temp[3] . "</p>";
 
-                        echo "<p class='seller-license'><strong>License: </strong>" . $temp[5] . "</p>";
-                        echo "<p class='seller-license'><strong>Car Make/Model: </strong>" . $temp[8] . "</p>";
-                        echo "<p class='seller-license'><strong>Car Year: </strong>" . $temp[9] . "</p>";
-                    echo "</div>";
-                    echo "<div class='col-sm-12 col-md-6'>";
-                        echo "<button class='btn btn-info express-interest' type='button' data-toggle='modal' data-target='#myModal'>Express Interest</button>";
+                                if($userSearch[3]) {
+                                    echo "<p class='seller-contact'><strong>Contact Number: </strong>" . $temp[4] . "</p>";
+                                    echo "<p class='seller-owner-count'><strong>Owner Count: </strong>" . $temp[6] . "</p>";
+                                    echo "<p class='seller-service'><strong>Service List: </strong>" . $temp[7] . "</p>";
+                                }
+
+                                echo "<p class='seller-license'><strong>License: </strong>" . $temp[5] . "</p>";
+                                echo "<p class='seller-license'><strong>Make/Model: </strong>" . $temp[8] . "</p>";
+                                echo "<p class='seller-license'><strong>Year: </strong>" . $temp[9] . "</p>";
+                            echo "</div>";
+                        echo "</div>";
+                        echo "<div class='col-sm-12 col-md-6'>";
+                            echo "<button class='btn btn-info express-interest' type='button' data-toggle='modal' data-target='#myModal'>Express Interest</button>";
+                        echo "</div>";
                     echo "</div>";
                 echo "</div>";
-            echo "</div>";
 
-            unset($temp);
+                unset($temp);
+
+                $resCount++;
+
+            endif;
 
 
         }
 
+        if($resCount == 0){
+            echo "<p>Your query didn't return any results</p>";
+            echo "<p>Please try your <a href='http://isit.local/ass2'>search again</a></p>";
+        }
+
+    }
+
+    function check_record($details, $userSearch){
+
+        $displayResult = false;
+        $percent;
+
+        $licenseRes;
+        $userRes;
+
+        if(all_fields($userSearch)){
+            if($userSearch[0] == $details[5] && $userSearch[1] == $details[6]){
+                similar_text($userSearch[2],$details[7], $percent);
+                if($percent > 50){
+                    return true;
+                }else {
+                    return false;
+                }
+            }else {
+                return false;
+            }
+
+        }else {
+
+            if(empty($userSearch[0]) && empty($userSearch[1]) && empty($userSearch[2])){
+                return true;
+            }
+
+            $licenseRes = license_check($userSearch[0], $details[5]);
+            $userRes = check_users($userSearch[1], $details[6]);
+            $serviceRes = compare_service($userSearch[2], $details[7]);
+
+            if($licenseRes && $userRes && $serviceRes){
+                return true;
+            }else {
+                return false;
+            }
+
+        }
+
+    }
+
+    function all_fields($userSearch){
+
+        $allFields = false;
+        $counter = 0;
+
+        for($i = 0; $i < sizeof($userSearch) -1; $i++){
+
+            if(!empty($userSearch[$i])){
+                $counter++;
+            }
+
+        }
+
+        // if all fields are filled in return true
+        if($counter == 3){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    // check the registration - license number
+    function license_check($licenseVal,$catLicense){
+        if (strlen($licenseVal) > 0) {
+
+            if ($licenseVal == $catLicense) {
+                return true;
+            }else {
+                return false;
+            }
+        }else {
+            return true;
+        }
+    }
+
+    //check the user number
+    function check_users($userVal, $catUser){
+        if (strlen($userVal) > 0) {
+            if ($userVal == $catUser) {
+                return true;
+            } else {
+                return false;
+            }
+        }else {
+            return true;
+        }
+    }
+
+    // compare service string
+    function compare_service($userService, $catService){
+
+        $percent;
+
+        if (strlen($userService) > 0) {
+            similar_text($userService, $catService, $percent);
+            if($percent > 50){
+                return true;
+            }else {
+                return false;
+            }
+        }else {
+            return true;
+        }
     }
 
 ?>
