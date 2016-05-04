6300d0e568e8e6321fd502105e2dededf47a7347
geomsaek <msaliba.6@gmail.com>
2016-05-04 16:25:52 +1000

    task2 complete

---
+++ ass7/task2/inc/nav.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-default">
+    <div id="navbar" class="navbar-collapse collapse">
+        <ul class="nav navbar-nav">
+            <li class="active"><a href="http://isit.local/ass7/task2/profile.php">My Home</a></li>
+            <li><a href="http://isit.local/ass7/task2/exam-select.php">Exam</a></li>
+            <li><a href="http://isit.local/ass7/task2/task2b.php?logout=true">Logout</a></li>
+        </ul>
+    </div>
+</nav>
